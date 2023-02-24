f89107d6139b2a59fd9cbe5a49846529d01ebdce
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-02-23 20:31:52 -0800

    added starter code for server.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,8 @@ const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
 
+const postData = require('./postData.json');
+
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
