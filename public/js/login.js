5aee5368e454305eea3bdfb0398e0631a1de4b09
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-03-02 18:26:15 -0800

    added changes to handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -34,7 +34,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Failed to sign up.');
     }
