a0b8ad9fe4dd5852336add3afa07c2c2e0d20a73
Jordan Dukes <jordandukes@Jordans-MBP.attlocal.net>
2023-03-02 19:45:00 -0600

    required cloudinary in upload.js

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,8 +1,5 @@
-import { v2 as cloudinary } from 'cloudinary';
+const cloudinary = require('../utils/cloudinary')
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+//permits uploads to cloudinary account
+cloudinary.uploader.upload
 
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,7 +3,8 @@ const cloudinary = require ('cloudinary').v2;
 cloudinary.config ({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_KEY,
-    api_secret: process.env.CLOUD_KEY_SECRET
+    api_secret: process.env.CLOUD_KEY_SECRET,
+    secure: true
 });
 
 module.exports = cloudinary;
