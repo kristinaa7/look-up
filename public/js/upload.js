b4d3484ed22b87ea22a070f51bb678b46ece9783
Kristina Alabastro <kristinaa7@live.com>
2023-03-05 17:08:16 -0800

    added script js to main handlebars

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,23 +1,20 @@
-// import { v2 as cloudinary } from 'cloudinary';
-
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-form.addEventListener("submit", (e) => {
+const pictureUpload = document.getElementById('pictureUpload')
+console.log('pictureupload', pictureUpload);
+pictureUpload.addEventListener("submit", (e) => {
     e.preventDefault();
   
     const files = document.querySelector("[type=file]").files;
     const formData = new FormData();
-  
+
     for (let i = 0; i < files.length; i++) {
       let file = files[i];
       formData.append("file", file);
       formData.append("upload_preset", "docs_upload_example_us_preset");
   
-      fetch(url, {
+      console.log('form data', formData)
+
+      fetch('/api/posts', {
+        
         method: "POST",
         body: formData
       })
