0bb33b854b71e325604fa48b4ca41cfe481dbed2
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-03-05 19:31:34 -0800

    uncomment codes

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,7 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
   if (!req.session.logged_in) {
-    // res.redirect('/login');
-    console.log("auth fail");
+    res.redirect('/login');
   } else {
     next();
   }
